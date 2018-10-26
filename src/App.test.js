0c87cbe0e91669064f49806a4f07ba81c4b60f7f
Mike <sanders.michael.j@gmail.com>
2018-10-26 14:53:28 -0400

    added test configurations

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('app', () => {
+  it('should find my references', () => {
+    const app = shallow(<App/>);
+
+    expect(app.find('img')).toBeTruthy();
+  });
 });
